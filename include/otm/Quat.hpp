db6e340fabba7ec7432a9c790aebf90c764bb022
Othereum <seokjin.dev@gmail.com>
2020-05-02 18:06:31 +0900

    feat: Invert

--- include/otm/Quat.hpp
+++ include/otm/Quat.hpp
@@ -1,7 +1,5 @@
 #pragma once
-#include "Angle.hpp"
 #include "MathUtil.hpp"
-#include "Vector.hpp"
 
 namespace otm
 {
@@ -17,11 +15,20 @@ namespace otm
 		constexpr Quat() noexcept: v{}, s{1} {}
 		constexpr Quat(const Vec3& v, float s) noexcept: v{v}, s{s} {}
 		constexpr Quat(float x, float y, float z, float w) noexcept: x{x}, y{y}, z{z}, w{w} {}
-		constexpr Quat(const Vec4& v4) noexcept: v4{v4} {}
+		explicit constexpr Quat(const Vec4& v4) noexcept: v4{v4} {}
 		
 		Quat(const UVec3& axis, Radians angle) noexcept
 			:Quat{axis.Get() * Sin(angle / 2), Cos(angle / 2)}
 		{
 		}
+
+		constexpr Quat& operator*=(const Quat& q) noexcept { return *this = *this * q; }
+		constexpr Quat operator*(const Quat& q) const noexcept
+		{
+			return {s*q.v + q.s*v + (v^q.v), s*q.s - (v|q.v)};
+		}
+
+		constexpr void Invert() noexcept { v.Negate(); }
+		constexpr Quat operator~() const noexcept { return {-v, s}; }
 	};
 }
