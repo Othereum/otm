c3a9917d38728bea604a8167c79ab7aa9a344e6e
Othereum <seokjin.dev@gmail.com>
2020-05-14 15:12:57 +0900

    feat: Support generic types

--- include/otm/Angle.hpp
+++ include/otm/Angle.hpp
@@ -4,13 +4,13 @@
 
 namespace otm
 {
-	template <class R>
+	template <class R, class F = float>
 	struct Angle
 	{
-		static constexpr auto ratio = static_cast<float>(R::num) / R::den;
+		static constexpr auto ratio = static_cast<F>(R::num) / R::den;
 
 		constexpr Angle() noexcept = default;
-		constexpr explicit Angle(float r) noexcept :val{r} {}
+		constexpr explicit Angle(F r) noexcept :val{r} {}
 		
 		template <class S>
 		constexpr Angle(const Angle<S>& r) noexcept { *this = r; }
@@ -32,11 +32,11 @@ namespace otm
 		constexpr Angle operator-(const Angle<S>& r) const noexcept { return *this - Angle{r}; }
 		constexpr Angle operator-(const Angle& r) const noexcept { return Angle{val - r.Get()}; }
 
-		constexpr Angle& operator*=(float f) noexcept { return *this = *this * f; }
-		constexpr Angle operator*(float f) const noexcept { return Angle{val * f}; }
+		constexpr Angle& operator*=(F f) noexcept { return *this = *this * f; }
+		constexpr Angle operator*(F f) const noexcept { return Angle{val * f}; }
 		
-		constexpr Angle& operator/=(float f) noexcept { return *this = *this / f; }
-		constexpr Angle operator/(float f) const noexcept { return Angle{val / f}; }
+		constexpr Angle& operator/=(F f) noexcept { return *this = *this / f; }
+		constexpr Angle operator/(F f) const noexcept { return Angle{val / f}; }
 
 		constexpr Angle operator-() const noexcept { return Angle{-val}; }
 
@@ -44,15 +44,18 @@ namespace otm
 		constexpr auto operator<=>(const Angle<S>& r) const noexcept { return *this <=> Angle{r}; }
 		constexpr auto operator<=>(const Angle&) const noexcept = default;
 
-		[[nodiscard]] constexpr float Get() const noexcept { return val; }
-		explicit constexpr operator float() const noexcept { return val; }
+		[[nodiscard]] constexpr F Get() const noexcept { return val; }
+		explicit constexpr operator F() const noexcept { return val; }
 
 	private:
-		float val = 0.f;
+		F val = 0;
 	};
 
-	using Radians = Angle<std::ratio<PiRatio::num, PiRatio::den * 180>>;
-	using Degrees = Angle<std::ratio<1>>;
+	using RadianRatio = std::ratio<PiRatio::num, PiRatio::den * 180>;
+	using DegreeRatio = std::ratio<1>;
+
+	using Radians = Angle<RadianRatio>;
+	using Degrees = Angle<DegreeRatio>;
 
 	constexpr Radians operator""_rad(unsigned long long f) noexcept { return Radians{static_cast<float>(f)}; }
 	constexpr Radians operator""_rad(long double f) noexcept { return Radians{static_cast<float>(f)}; }
@@ -60,8 +63,8 @@ namespace otm
 	constexpr Degrees operator""_deg(unsigned long long f) noexcept { return Degrees{static_cast<float>(f)}; }
 	constexpr Degrees operator""_deg(long double f) noexcept { return Degrees{static_cast<float>(f)}; }
 	
-	template <class R>
-	Angle<R> operator*(float f, const Angle<R>& r) noexcept
+	template <class R, class F, class F2>
+	Angle<R, F> operator*(F2 f, const Angle<R, F>& r) noexcept
 	{
 		return r * f;
 	}
