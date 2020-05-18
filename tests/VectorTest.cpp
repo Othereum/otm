f75658735426e74aeb8a34baa0fbd7a02d514cec
Othereum <seokjin.dev@gmail.com>
2020-05-19 04:05:58 +0900

    feat: constexpr

--- tests/VectorTest.cpp
+++ tests/VectorTest.cpp
@@ -5,14 +5,14 @@ using namespace otm;
 
 TEST(VectorTest, Constructor)
 {
-	Vec3 v1{1, 1, 1};
+	constexpr Vec3 v1{1, 1, 1};
 	EXPECT_EQ(v1.x, 1);
 	EXPECT_EQ(v1.y, 1);
 	EXPECT_EQ(v1.z, 1);
 
-	auto v2 = Vec3::One();
+	constexpr auto v2 = Vec3::One();
 	EXPECT_EQ(v1, v2);
 
-	Vec3 v3{All{}, 1};
+	constexpr Vec3 v3{All{}, 1};
 	EXPECT_EQ(v2, v3);
 }
