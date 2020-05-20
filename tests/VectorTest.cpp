67599eb9ab63b0de50813b75faeca70ef09a5934
Othereum <seokjin.dev@gmail.com>
2020-05-20 09:58:55 +0900

    test: Add VectorTest::Ext

--- tests/VectorTest.cpp
+++ tests/VectorTest.cpp
@@ -50,3 +50,13 @@ TEST(VectorTest, Operations)
 	EXPECT_NEAR(v4.DistSqr(v2), 86, kSmallNumber);
 	EXPECT_NEAR(v4.Dist(v2), 9.273618495495704f, kSmallNumber);
 }
+
+TEST(VectorTest, Ext)
+{
+	const auto v1 = Vec3::Rand(0, 1);
+	auto pred = [](float f) { return f >= 0 && f < 1; };
+	EXPECT_TRUE(std::all_of(v1.begin(), v1.end(), pred));
+	
+	auto v2 = UVec3::Rand();
+	EXPECT_NEAR(v2.Get().LenSqr(), 1, kSmallNumber);
+}
