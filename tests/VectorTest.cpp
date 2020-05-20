f627b1fb73bed0b0f6012ecfe25a4cd6cd0f9624
Othereum <seokjin.dev@gmail.com>
2020-05-20 20:01:30 +0900

    test: Add test of Normalize(nothrow_t) and cross product

--- tests/VectorTest.cpp
+++ tests/VectorTest.cpp
@@ -19,36 +19,42 @@ TEST(VectorTest, Basic)
 
 	Vec4 v4;
 	v4.Assign(v1, 2);
-	EXPECT_NEAR(v4.x, 0, kSmallNumber);
-	EXPECT_NEAR(v4.y, 0, kSmallNumber);
-	EXPECT_NEAR(v4.z, 1, kSmallNumber);
-	EXPECT_NEAR(v4.w, 1, kSmallNumber);
+	EXPECT_NEAR(v4.x, 0, kSmallNum);
+	EXPECT_NEAR(v4.y, 0, kSmallNum);
+	EXPECT_NEAR(v4.z, 1, kSmallNum);
+	EXPECT_NEAR(v4.w, 1, kSmallNum);
 
 	v4.Assign(v1, -2);
-	EXPECT_NEAR(v4.x, 1, kSmallNumber);
-	EXPECT_NEAR(v4.y, 0, kSmallNumber);
-	EXPECT_NEAR(v4.z, 1, kSmallNumber);
-	EXPECT_NEAR(v4.w, 1, kSmallNumber);
+	EXPECT_NEAR(v4.x, 1, kSmallNum);
+	EXPECT_NEAR(v4.y, 0, kSmallNum);
+	EXPECT_NEAR(v4.z, 1, kSmallNum);
+	EXPECT_NEAR(v4.w, 1, kSmallNum);
 }
 
 TEST(VectorTest, Operations)
 {
 	Vec4 v1;
 	EXPECT_THROW(v1.Normalize(), DivByZero);
-
+	EXPECT_FALSE(v1.Normalize(std::nothrow));
+	
 	constexpr Vec3 v2{1, 1, 1};
 	const Vec3 v3 = v2.Unit();
 	constexpr auto f = 0.5773502691896258f;
-	EXPECT_NEAR(v3.x, f, kSmallNumber);
-	EXPECT_NEAR(v3.y, f, kSmallNumber);
-	EXPECT_NEAR(v3.z, f, kSmallNumber);
+	EXPECT_NEAR(v3.x, f, kSmallNum);
+	EXPECT_NEAR(v3.y, f, kSmallNum);
+	EXPECT_NEAR(v3.z, f, kSmallNum);
 
-	EXPECT_NEAR(v2.LenSqr(), 3, kSmallNumber);
-	EXPECT_NEAR(v2.Len(), 1.732050807568877f, kSmallNumber);
+	EXPECT_NEAR(v2.LenSqr(), 3, kSmallNum);
+	EXPECT_NEAR(v2.Len(), 1.732050807568877f, kSmallNum);
 
 	constexpr Vec3 v4{3, 10, 2};
-	EXPECT_NEAR(v4.DistSqr(v2), 86, kSmallNumber);
-	EXPECT_NEAR(v4.Dist(v2), 9.273618495495704f, kSmallNumber);
+	EXPECT_NEAR(v4.DistSqr(v2), 86, kSmallNum);
+	EXPECT_NEAR(v4.Dist(v2), 9.273618495495704f, kSmallNum);
+
+	constexpr Vector cp1{2, 5, 1}, cp2{3, 1, 7};
+	constexpr auto cpr = cp1 ^ cp2;
+	constexpr Vector cpe{34, -11, -13};
+	EXPECT_EQ(cpr, cpe);
 }
 
 TEST(VectorTest, Ext)
@@ -58,5 +64,5 @@ TEST(VectorTest, Ext)
 	EXPECT_TRUE(std::all_of(v1.begin(), v1.end(), pred));
 	
 	auto v2 = UVec3::Rand();
-	EXPECT_NEAR(v2.Get().LenSqr(), 1, kSmallNumber);
+	EXPECT_NEAR(v2.Get().LenSqr(), 1, kSmallNum);
 }
