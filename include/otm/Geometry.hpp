37acd94119f04a5c863c87ea25810751971ff0fa
Othereum <seokjin.dev@gmail.com>
2020-05-25 12:54:04 +0900

    feat: Custom length

--- include/otm/Geometry.hpp
+++ include/otm/Geometry.hpp
@@ -4,47 +4,64 @@
 
 namespace otm
 {
-	template <std::floating_point T>
-	constexpr Matrix<T, 3, 3> MakeRotation(const Quaternion<T>& rotation) noexcept
+	template <size_t L = 3, std::floating_point T = float>
+	constexpr Matrix<T, L> MakeRotation(const Quaternion<T>& rotation) noexcept
 	{
+		static_assert(L >= 3);
+		
 		const auto& [x, y, z] = rotation.v.data;
 		const auto& w = rotation.s;
+
+		Matrix<T, L> m;
+		m[0] << 1 - 2*(y*y + z*z) << 2*(x*y + w*z) << 2*(x*z - w*y);
+		m[1] << 2*(x*y - w*z) << 1 - 2*(x*x + z*z) << 2*(y*z + w*x);
+		m[2] << 2*(x*z + w*y) << 2*(y*z - w*x) << 1 - 2*(x*x + y*y);
+
+		for (size_t i=3; i<L; ++i)
+			m[i][i] = 1;
 		
-		return {
-			1 - 2*(y*y + z*z),	2*(x*y + w*z),		2*(x*z - w*y),
-			2*(x*y - w*z),		1 - 2*(x*x + z*z),	2*(y*z + w*x),
-			2*(x*z + w*y),		2*(y*z - w*x),		1 - 2*(x*x + y*y)
-		};
+		return m;
 	}
 
-	template <class T>
-	constexpr Matrix<T, 3, 3> MakeScale(const Vector<T, 3>& scale) noexcept
+	template <size_t L = 3, class T = float>
+	constexpr Matrix<T, L> MakeScale(const Vector<T, 3>& scale) noexcept
 	{
-		Matrix<T, 3, 3> s;
+		static_assert(L >= 3);
+		
+		Matrix<T, L> s;
 		s[0][0] = scale[0];
 		s[1][1] = scale[1];
 		s[2][2] = scale[2];
+		
+		for (size_t i=3; i<L; ++i)
+			s[i][i] = 1;
+		
 		return s;
 	}
 	
-	template <class T>
-	constexpr Matrix<T, 4, 4> MakeTranslation(const Vector<T, 3>& pos) noexcept
+	template <class T = float>
+	constexpr Matrix<T, 4> MakeTranslation(const Vector<T, 3>& pos) noexcept
 	{
-		return Matrix<T, 4, 4>::Identity(pos.ToRowMatrix(), {0, 3});
+		return Matrix<T, 4>::Identity(pos.ToRowMatrix(), {0, 3});
 	}
 
-	template <class T>
-	constexpr Matrix<CommonFloat<T>, 3, 3> MakeSimpleViewProj(const Vector<T, 2>& screen) noexcept
+	template <size_t L = 2, class T = float>
+	constexpr Matrix<CommonFloat<T>, L> MakeSimpleViewProj(const Vector<T, 2>& screen) noexcept
 	{
-		Matrix<CommonFloat<T>, 3, 3> proj;
+		static_assert(L >= 2);
+		
+		Matrix<CommonFloat<T>, L> proj;
 		proj[0][0] = 2 / ToFloat(screen[0]);
 		proj[1][1] = 2 / ToFloat(screen[1]);
-		proj[2][2] = 1;
+		
+		for (size_t i=2; i<L; ++i)
+			proj[i][i] = 1;
+		
 		return proj;
 	}
 
-	template <std::floating_point T>
-	static Matrix<T, 4, 4> MakeLookAt(const Vector<T, 3>& eye, const Vector<T, 3>& target, const Vector<T, 3>& up)
+	template <std::floating_point T = float>
+	static Matrix<T, 4> MakeLookAt(const Vector<T, 3>& eye, const Vector<T, 3>& target, const Vector<T, 3>& up)
 	{
 		auto k = target - eye; k.Normalize();
 		auto i = up ^ k; i.Normalize();
