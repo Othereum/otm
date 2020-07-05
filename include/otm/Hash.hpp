d3ca7f9e9e60246761e003bb22012a42abfaf2f5
Othereum <seokjin.dev@gmail.com>
2020-07-05 10:54:17 +0900

    fix: downgrade to C++17

--- include/otm/Hash.hpp
+++ include/otm/Hash.hpp
@@ -1,12 +1,11 @@
 #pragma once
-#include <iterator>
 
 namespace otm
 {
 	constexpr auto kHashOffsetBasis = []{ if constexpr(sizeof(size_t) >= 8) { return 14695981039346656037ULL; } else { return 2166136261U; } }();
 	constexpr auto kHashPrime = []{ if constexpr(sizeof(size_t) >= 8) { return 1099511628211ULL; } else { return 16777619U; } }();
 
-	template <std::input_iterator It, std::invocable<typename std::iterator_traits<It>::value_type> Fn>
+	template <class It, class Fn>
 	constexpr size_t HashRange(size_t val, It first, It last, Fn modifier) noexcept
 	{
 	    for (; first != last; ++first)
@@ -17,19 +16,19 @@ namespace otm
 	    return val;
 	}
 
-	template <std::input_iterator It>
+	template <class It>
 	constexpr size_t HashRange(size_t val, It first, It last) noexcept
 	{
 	    return HashRange(val, first, last, []<class T>(T&& x){return std::forward<T>(x);});
 	}
 
-	template <std::input_iterator It, std::invocable<typename std::iterator_traits<It>::value_type> Fn>
+	template <class It, class Fn>
 	constexpr size_t HashRange(It first, It last, Fn modifier) noexcept
 	{
 		return HashRange(kHashOffsetBasis, first, last, modifier);
 	}
 
-	template <std::input_iterator It>
+	template <class It>
 	constexpr size_t HashRange(It first, It last) noexcept
 	{
 		return HashRange(kHashOffsetBasis, first, last);
